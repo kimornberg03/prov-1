8b254c9a1b509aec6d437ce77f012ffe28f77a50
unknown <kim.ornberg@skola.botkyrka.se>
2020-10-19 15:38:21 +0200

    uppgift 10

---
+++ 10/script.js
@@ -0,0 +1,26 @@
+let numbers = [12, 13, 15, 23]
+
+console.log(delbart(numbers))
+
+
+
+
+function delbart(array) {
+    let newArr = [3]
+
+}
+
+
+{
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+
+    }
+
+    if (x % 3 == 0) {
+        x + newArr
+    } else {
+
+    }
+}
